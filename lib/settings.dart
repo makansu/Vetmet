19e9c7474eeba3982cdd72f9c7facab018bd9fe3
makansu <siwillow48@gmail.com>
2025-03-14 20:44:17 +0300

    Update lib

---
+++ lib/settings.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Ayarlar')),
+      body: Center(child: Text('Ayarlar Ekranı')),
+    );
+  }
+}
